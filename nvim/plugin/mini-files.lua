60a7b8089f8c81cb5f3937e22ee135570a37f01d
Jun-te Kim <junte.kim@mealcraft.com>
2025-09-06 21:52:42 +0000

    added mini-files option

---
+++ nvim/plugin/mini-files.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.files",
+  opts = {
+    mappings = {
+      ["<Up>"] = "k",
+      ["<Down>"] = "j",
+      ["<Left>"] = "h",
+      ["<Right>"] = "l",
+    },
+  },
+}
